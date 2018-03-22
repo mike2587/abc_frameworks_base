88d2acc99110509fdd94420d2f401adf9190271e
xyyx <xyyx@mail.ru>
2018-03-22 16:30:41 +0100

    Circle battery: show power saving indicator
    Change-Id: I4a2bd457a18c102ee1f1986a32c1f127c7cb5d2c
    abc: tune up the "plus" size

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -91,6 +91,8 @@ public class BatteryMeterView extends LinearLayout implements
     public static final int STYLE_CLOCK_RIGHT = 0;
     public static final int STYLE_CLOCK_LEFT = 1;
 
+    private boolean mPowerSave;
+
     public BatteryMeterView(Context context) {
         this(context, null, 0);
     }
@@ -148,7 +150,7 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     public void setForceShowPercent(boolean show) {
-        mForceShowPercent = show;
+        mForceShowPercent = show || mPowerSave;
         updateShowPercent();
     }
 
@@ -220,6 +222,9 @@ public class BatteryMeterView extends LinearLayout implements
     @Override
     public void onPowerSaveChanged(boolean isPowerSave) {
         mDrawable.setPowerSave(isPowerSave);
+        mPowerSave = isPowerSave;
+        mForceShowPercent = isPowerSave;
+        updateShowPercent();
     }
 
     private TextView loadPercentView() {
@@ -377,7 +382,7 @@ public class BatteryMeterView extends LinearLayout implements
 
         if (forcePercentageQsHeader()
                 || style == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT
-                || ((isCircleBattery() || style == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT) && mCharging)) {
+                || ((isCircleBattery() || style == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT) && (mCharging || mPowerSave))) {
             mForceShowPercent = true;
         } else {
             mForceShowPercent = false;
